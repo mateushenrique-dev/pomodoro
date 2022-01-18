59b98ac515889c5ccfafb6ad0d7ed2fe079be503
Mateus Henrique <mhenriqq32@gmail.com>
2022-01-18 11:56:22 -0300

    progress circle responsivity

--- src/components/Clock/ProgressCircle/index.tsx
+++ src/components/Clock/ProgressCircle/index.tsx
@@ -1,43 +1,15 @@
-import { useEffect, useState } from "react";
 import { Colors } from "../../../context/modal";
+import styles from "./progress-circle.module.scss"
 
 const ProgressCircle = ({ color }: { color: Colors }) => {
-  const screenWidth = window.screen.width;
-  const [progressProperty, setProgressProperty] = useState({
-    width: 248,
-    height: 248,
-    strokeWidth: 5,
-    cx: 125,
-    cy: 125,
-    r: 110,
-  });
-
-  useEffect(() => {
-    console.log(screenWidth);
-    if (screenWidth > 457) {
-      setProgressProperty({
-        width: 366,
-        height: 366,
-        strokeWidth: 15,
-        cx: 183,
-        cy: 185,
-        r: 160,
-      });
-    }
-  }, [screenWidth]);
-
   return (
-    <svg
-      className="progress"
-      width={progressProperty.width}
-      height={progressProperty.height}
-    >
+    <svg width={248} height={248} viewBox="0 0 250 250" className={styles.progress}>
       <circle
-        cx={progressProperty.cx}
-        cy={progressProperty.cy}
-        r={progressProperty.r}
+        cx={125}
+        cy={125}
+        r={110}
         stroke={color}
-        strokeWidth={progressProperty.strokeWidth}
+        strokeWidth={5}
         fill="transparent"
         style={{
           strokeDashoffset: 0,
